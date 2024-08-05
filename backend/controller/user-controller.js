121c2ad2ef72d52e3299e8ef96f217a75baf9dc5
Anjali <anjalikumari2907@gmail.com>
2024-08-05 13:01:44 +0530

    user info in db

--- backend/controller/user-controller.js
+++ backend/controller/user-controller.js
@@ -1,4 +1,17 @@
+import User from "../model/user-model.js";
 
-export const addUser = (req , res) => {
-     console.log(req.body)
-}
+export const addUser = async (req, res) => {
+    // console.log(req.body)
+    try {
+        const exist = await User.findOne({ sub: req.body.sub });
+        if (exist) {
+            res.status(200).json({ message: "User already exist" });
+            return;
+        }
+        const newUser = new User(req.body);
+        await newUser.save();
+        return res.status(200).json(newUser);
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+};
